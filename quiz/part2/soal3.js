64d25091d31f5fea98c37f0910a024894b690b94
YudaClairee <djibyuda@gmail.com>
2025-05-27 18:31:48 +0700

    mengerjakan part 2

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,12 @@
+let word3 = "wow JavaScript is so cool";
+let exampleFirstWord3 = word3.substring(0, 3);
+let secondWord3 = word3.substring(4, 14);
+let thirdWord3 = word3.substring(15, 17);
+let fourthWord3 = word3.substring(18, 20);
+let fifthWord3 = word3.substring(21, 25);
+
+console.log("First Word: " + exampleFirstWord3);
+console.log("Second Word: " + secondWord3);
+console.log("First Word: " + thirdWord3);
+console.log("First Word: " + fourthWord3);
+console.log("First Word: " + fifthWord3);
---
+++ quiz/part2/soal4.js
@@ -0,0 +1,23 @@
+let word3 = "wow JavaScript is so cool";
+let exampleFirstWord3 = word3.substring(0, 3);
+let firstWordLength = exampleFirstWord3.length;
+let secondWord3 = word3.substring(4, 14);
+let secondWordLength = secondWord3.length;
+let thirdWord3 = word3.substring(15, 17);
+let thirdWordLength = thirdWord3.length;
+let fourthWord3 = word3.substring(18, 20);
+let fourthWordLength = fourthWord3.length;
+let fifthWord3 = word3.substring(21, 25);
+let fifthWordLength = fifthWord3.length;
+
+console.log(
+  "First Word: " + exampleFirstWord3 + ", with length: " + firstWordLength
+);
+console.log(
+  "Second Word: " + secondWord3 + ", with length: " + secondWordLength
+);
+console.log("Third Word: " + thirdWord3 + ", with length: " + thirdWordLength);
+console.log(
+  "Fourth Word: " + fourthWord3 + ", with length: " + fourthWordLength
+);
+console.log("Fifth Word: " + fifthWord3 + ", with length: " + fifthWordLength);
